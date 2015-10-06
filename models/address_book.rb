2a4b5711b48b8ff1da4f9007c99af95bde2bc7df
bpschmidt <musicchick5285@yahoo.com>
2015-10-06 17:29:04 +0000

    assignment-23

--- models/address_book.rb
+++ models/address_book.rb
@@ -21,8 +21,14 @@ class AddressBook
      @entries.insert(index, Entry.new(name, phone, email))
    end
    
-    def remove_entry(name, phone, email)
-       @entries.delete_if { |entry| Entry.new(name, phone, email) == entry } 
+    def remove_entry(name, phone_number, email)
+        # Going to leave notes in line...
+        # I initially thought this would work too. But it doesn't...
+        # the == method only returns true if they are the exact same object in this case.
+        # Since they are technically two different objects in memory, this will not find the match.
+        # try checkint email against entry.email, and phone against entry.phone. It's more verbose,
+        # but it should get the job done!
+       @entries.delete_if { |entry| name == entry.name && phone_number == entry.phone_number && email == entry.email } 
     end
 end
 

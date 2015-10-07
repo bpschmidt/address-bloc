9b35120ca9501698ec4f19ee4ac26c1cfe8ba310
bpschmidt <musicchick5285@yahoo.com>
2015-10-07 16:36:55 +0000

    Assignment 25 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -34,12 +34,6 @@ class AddressBook
    end
    
     def remove_entry(name, phone_number, email)
-        # Going to leave notes in line...
-        # I initially thought this would work too. But it doesn't...
-        # the == method only returns true if they are the exact same object in this case.
-        # Since they are technically two different objects in memory, this will not find the match.
-        # try checkint email against entry.email, and phone against entry.phone. It's more verbose,
-        # but it should get the job done!
        @entries.delete_if { |entry| name == entry.name && phone_number == entry.phone_number && email == entry.email } 
     end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -117,9 +117,45 @@ require_relative '../models/address_book.rb'
        expect(book.entries.size).to eq 1
       
       end
+      
+   context ".import_from_entries_2.csv" do
+     it "imports the correct number of entries from entries_2.csv" do
+ 
+       book.import_from_csv("entries_2.csv")
+       book_size = book.entries.size
+ 
+       
+       expect(book_size).to eql 3
+     end
+     
+     it "imports the 1st entry from entries_2.csv" do
+       book.import_from_csv("entries_2.csv")
+       
+       entry_one = book.entries[0]
+       
+       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+       
+     end
+     
+     it "imports the 2nd entry from entries_2.csv" do
+       book.import_from_csv("entries_2.csv")
+       # Check the second entry
+       entry_two = book.entries[1]
+       
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+
+     end
+ 
+     it "imports the 3rd entry from entries_2.csv" do
+       book.import_from_csv("entries_2.csv")
+       # Check the third entry
+       entry_three = book.entries[2]
+       
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
+  end
+ end
+end
     
        
      
-     end
-   
- end

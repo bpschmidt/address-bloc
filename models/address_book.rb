2435a11d69b458e8f350b7259cf42040414c44df
Brenda Schmidt <musicchick5285@yahoo.com>
2015-10-07 21:07:40 -0400

    Assignment 26 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,36 +3,36 @@ require "csv"
 
 class AddressBook
     attr_accessor :entries
-    
+
     def initialize
         @entries = []
     end
-      
+
    def add_entry(name, phone, email)
-       
+
         index = 0
      @entries.each do |entry|
- 
+
        if name < entry.name
          break
        end
        index += 1
      end
- 
+
      @entries.insert(index, Entry.new(name, phone, email))
    end
-   
+
    def import_from_csv(file_name)
      csv_text = File.read(file_name)
      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
- 
+
      csv.each do |row|
        row_hash = row.to_hash
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
- 
+
    end
-   
+
     def remove_entry(name, phone_number, email)
         # Going to leave notes in line...
         # I initially thought this would work too. But it doesn't...
@@ -40,9 +40,9 @@ class AddressBook
         # Since they are technically two different objects in memory, this will not find the match.
         # try checkint email against entry.email, and phone against entry.phone. It's more verbose,
         # but it should get the job done!
-       @entries.delete_if { |entry| name == entry.name && phone_number == entry.phone_number && email == entry.email } 
+       @entries.delete_if { |entry| name == entry.name && phone_number == entry.phone_number && email == entry.email }
     end
-    
+
     def remove_entry_2(name, phone_number, email)
         @entries.each do |entry|
             if name == entry.name && phone_number == entry.phone_number && email == entry.email
@@ -51,18 +51,18 @@ class AddressBook
             end
         end
     end
-    
+
     def binary_search(name)
              lower = 0
      upper = entries.length - 1
 
 
      while lower <= upper
- 
+
        mid = (lower + upper) / 2
        mid_name = entries[mid].name
- 
- 
+
+
        if name == mid_name
          return entries[mid]
        elsif name < mid_name
@@ -73,6 +73,14 @@ class AddressBook
      end
      return nil
     end
-end
 
-   
+    def iterative_search(name)
+      @entries.each do |entry|
+        if name == entry.name
+          return entry
+        end
+    end
+          return nil
+    
+  end
+end
